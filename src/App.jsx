6cefb7b253f8b0e4f199210b82dcb79a9d5d6b77
Solah <solah.kyle.skye@gmail.com>
2024-06-21 23:07:25 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 export default function App() {
-  return (
+  return ( 
     <>
     <form className="new-item-form">
       <div className="form-row">
@@ -11,6 +11,15 @@ export default function App() {
       <button className="btn">Add</button>
     </form>
     <h1 className="header">To do List</h1>
+    <ul className="list">
+      <li>
+        <label>
+          <input type="checkbox"/>
+          Item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    </ul>
     </>
   );
 }
