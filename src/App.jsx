82c250a745d337e8a894998da9a5be92bfacf900
Solah <solah.kyle.skye@gmail.com>
2024-06-25 23:38:57 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  return ( 
+  const [newItem, setNewItem] = useState("");
+  setNewItem("");
+
+  return (
     <>
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item" />
-      </div>
-      <button className="btn">Add</button>
-    </form>
-    <h1 className="header">To do List</h1>
-    <ul className="list">
-      <li>
-        <label>
-          <input type="checkbox"/>
-          Item 1
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
-    </ul>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">To do List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
     </>
   );
 }
