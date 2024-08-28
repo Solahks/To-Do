9c2c33a7d92817fa3fd3b9918ef8bd4df78b1148
Solah <solah.kyle.skye@gmail.com>
2024-08-27 23:42:51 -0500

    daily quest progress

--- src/DailyQuestForm.jsx
+++ src/DailyQuestForm.jsx
@@ -1,27 +1,29 @@
+import { useState } from "react";
+
 export function NewDailyQuest({ onSubmit }) {
-    const [newItem, setNewQuest] = useState("")
-  
-    function handleSubmit(e) {
-      e.preventDefault()
-      if (newItem === "") return
-  
-      onSubmit(newItem)
-  
-      setNewQuest("")
-    }
-  
-    return (
-      <form onSubmit={handleSubmit} className="new-daily-quest">
-        <div className="form-row-one">
-          <label htmlFor="quest">Set Daily Quests</label>
-          <input
-            value={newItem}
-            onChange={e => setNewItem(e.target.value)}
-            type="text-one"
-            id="quest"
-          />
-        </div>
-        <button className="btn-one">Accept</button>
-      </form>
-    )
+  const [newQuest, setNewQuest] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (newQuest === "") return;
+
+    onSubmit(newItem);
+
+    setNewQuest("");
   }
+
+  return (
+    <form onSubmit={handleSubmit} className="new-daily-quest">
+      <div className="form-row-one">
+        <label htmlFor="quest">Set Daily Quests</label>
+        <input
+          value={newQuest}
+          onChange={(e) => setNew(e.target.value)}
+          type="text-one"
+          id="quest"
+        />
+      </div>
+      <button className="btn-one">Accept</button>
+    </form>
+  );
+}
